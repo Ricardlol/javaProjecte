f757f6bb643c1e3bc7b9f7bf8378e6e401bb0f99
daw2 <daw2@aula302-13>
2019-04-04 10:12:01 +0200

    interface connections and create clas clients

---
+++ src/app/model/Connectionsdb.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.model;
+
+/**
+ *
+ * @author daw2
+ */
+public interface Connectionsdb {
+    
+}
