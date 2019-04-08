85ea43ee57b87b2e14fed1b4e0739c869f1387f7
daw2 <daw2@aula302-13>
2019-04-08 12:19:31 +0200

    control de errorres

--- src/app/clients/ClientsController.java
+++ src/app/clients/ClientsController.java
@@ -7,7 +7,6 @@ package app.clients;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,6 +14,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javax.swing.JFrame;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuButton;
 
 /**
  * FXML Controller class
@@ -38,12 +41,11 @@ public class ClientsController implements Initializable{
     @FXML Label errorTextNac;
     @FXML Label errorTextOc;
     @FXML Label errorTextCiv;
-    
-    public ClientsController(){
-        
-    }
+    @FXML Label errorTextIncorrectEmail;
+    @FXML MenuButton optionDoc;
     
     private Stage stage;
+    
     /**
      * Initializes the controller class.
      * @param url
@@ -52,6 +54,94 @@ public class ClientsController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
        //oculta mensaje de error
+       ocultarMensajes();
+    }
+    
+    public void changeToDNI(){
+        optionDoc.setText("DNI");
+    }
+    
+    public void changeToPassport(){
+        optionDoc.setText("PASSPORT");
+    }
+    
+    public void changeToNIE(){
+        optionDoc.setText("NIE");
+    }
+    
+    public void btnGuardar(){
+        ocultarMensajes();
+        boolean campos = comprovarCampos();
+        if(campos){
+            System.out.println("A la base de datos !!!");
+        } else{
+            System.out.println("Pon los bien puto");
+        }
+    }
+    
+    private boolean comprovarCampos(){
+        boolean result= true;
+        result=comprovarDocumentacion();
+        if (client.getText().length()==0){
+            errorTextName.setVisible(true);
+            result= false;
+        }
+        if (tel.getText().length()==0){
+            errorTextTel.setVisible(true);
+            result= false;
+        }
+        if (email.getText().length()==0){
+            errorTextEmail.setVisible(true);
+            result= false;
+        }
+        if(email.getText().length()>0){
+            boolean mensaje=comprovarEmail(email.getText());
+            if(!mensaje){
+                errorTextIncorrectEmail.setVisible(true);
+                result= false;
+            }
+        }
+        if (nac.getText().length()==0){
+            errorTextNac.setVisible(true);
+            result= false;
+        }
+        if (ocupation.getText().length()==0){
+            errorTextOc.setVisible(true);
+            result= false;
+        }
+        if (status.getText().length()==0){
+            errorTextCiv.setVisible(true);
+            result= false;
+        }
+        return result;
+    }
+    
+    private boolean comprovarDocumentacion(){
+        boolean result=true;
+        if(documnetation.getText().length()==0 && optionDoc.getText().equals("----")){
+            errorTextDocumentation.setText("Rellena el campo i elige una opcion");
+            errorTextDocumentation.setVisible(true);
+            result = false;
+        }else if(documnetation.getText().length()==0){
+            errorTextDocumentation.setVisible(true);
+            result = false;
+        }else if(optionDoc.getText().equals("----")){
+            errorTextDocumentation.setText("Elige una opcion");
+            errorTextDocumentation.setVisible(true);
+            result = false;
+        }
+        /*if (result){
+            result = comprovarString(optionDoc.getText());
+            System.out.println(result);
+            if (result==false){
+                errorTextDocumentation.setText(optionDoc.getText()+" opcio incorrecta");
+                errorTextDocumentation.setVisible(true);
+            }
+        }*/
+        return result;
+    }
+    
+    private void ocultarMensajes(){
         errorTextDocumentation.setVisible(false);
         errorTextName.setVisible(false);
         errorTextTel.setVisible(false);
@@ -59,51 +149,21 @@ public class ClientsController implements Initializable{
         errorTextNac.setVisible(false);
         errorTextOc.setVisible(false);
         errorTextCiv.setVisible(false);
+        errorTextIncorrectEmail.setVisible(false);
+    }
+    
+    private boolean comprovarEmail(String email){
+        boolean result = false;
+        String emailPattern = "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@"+"[a-z0-9-]+(\\.[a-z0-9-])$";
+        Pattern pattern = Pattern.compile(emailPattern);
+        if (email != null){
+            Matcher matcher = pattern.matcher(email);
+            if (matcher.matches()) {
+              System.out.println("Válido");
+            }
+        }
         
-        //cuando cualquier de los  textfield gane foco 
-        //si existe mensaje de error visible -> se oculta
-        
-        documnetation.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-             if (newValue){
-                errorTextDocumentation.setVisible(false);
-             }
-        });
-        
-        client.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-             if (newValue){
-                errorTextName.setVisible(false);
-             }
-        });
-        
-        tel.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-             if (newValue){
-                errorTextName.setVisible(false);
-             }
-        });
-        
-        email.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-             if (newValue){
-                errorTextName.setVisible(false);
-             }
-        });
-        
-        nac.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-             if (newValue){
-                errorTextName.setVisible(false);
-             }
-        });
-        
-        ocupation.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-             if (newValue){
-                errorTextName.setVisible(false);
-             }
-        });
-        
-        status.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-             if (newValue){
-                errorTextName.setVisible(false);
-             }
-        });
+        return result;
     }
     public void setStage (Stage stage){
         this.stage = stage;
