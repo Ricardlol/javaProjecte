2c8a17e1c8805911b231a9ea8e7195b3d43658ef
daw2 <daw2@aula302-13>
2019-04-09 10:33:21 +0200

    connectiondb

--- src/app/model/Clients.java
+++ src/app/model/Clients.java
@@ -7,22 +7,38 @@ package app.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author daw2
  */
 public class Clients implements Connectionsdb{
-
+    
+    private Connection conn;
+    private String sSQL="";
+    
+    public Clients(){
+        conn = Connectionsdb.connectarMySQL();
+    }
+    
     @Override
     public void buscar() {
+        String connexString = Connectionsdb.getConnectionDB();
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void crear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        sSQL ="INSERT INTO cliente VALUES (53642474B, Ricard, España, 635640728, ricard@gmail.com, Practicas, Soltero)";
+        try {
+            PreparedStatement pstm = conn.prepareStatement(sSQL);
+        } catch (SQLException ex) {
+            System.out.println("Error al insertar");
+        }
     }
 
     @Override
--- src/app/model/Connectionsdb.java
+++ src/app/model/Connectionsdb.java
@@ -5,6 +5,9 @@
  */
 package app.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author daw2
@@ -14,4 +17,35 @@ public interface Connectionsdb {
     public void crear();
     public void modificar();
     public void eliminar();
+    public static Connection connectarMySQL(){
+        Connection conn = null;
+
+        try {
+            Class.forName(geDriver());
+            conn = DriverManager.getConnection(getConnectionDB(), getUserDB(), getPasswordBD());
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+    public static String getConnectionDB(){
+        String database = "java_project";
+        String hostname = "192.168.12.41";
+        //String port = "3306";
+        String url = "jdbc:mysql://" + hostname + "/" + database + "?useSSL=false";
+        System.out.println(url);
+        return url;
+    };
+
+    public static String geDriver(){
+        return "com.mysql.jdbc.Driver";
+    }
+    public static String getUserDB(){
+        return "root";
+    };
+    public static String getPasswordBD(){
+        return "";
+        
+    };
 }
