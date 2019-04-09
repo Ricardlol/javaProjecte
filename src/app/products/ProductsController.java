f033370adeeb7ac1d7f6df8b037d0735e4addf79
pepeolo <pepeolo13@gmail.com>
2019-04-09 05:43:01 +0200

    'vpepeolo01'

--- src/app/products/ProductsController.java
+++ src/app/products/ProductsController.java
@@ -7,8 +7,14 @@ package app.products;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javax.swing.JFrame;
 
 /**
  * FXML Controller class
@@ -16,19 +22,94 @@ import javafx.stage.Stage;
  * @author daw2
  */
 public class ProductsController implements Initializable {
-    
-    
-    private Stage stage;
-     
+
     /**
      * Initializes the controller class.
      */
+    @FXML Button btnOk;
+    @FXML TextField documnetation;
+    @FXML TextField client;
+    @FXML TextField tel;
+    @FXML TextField email;
+    @FXML TextField nac;
+    @FXML TextField ocupation;
+    @FXML TextField status;
+    @FXML Label errorTextDocumentation;
+    @FXML Label errorTextName;
+    @FXML Label errorTextTel;
+    @FXML Label errorTextEmail;
+    @FXML Label errorTextNac;
+    @FXML Label errorTextOc;
+    @FXML Label errorTextCiv;
+    
+    public ProductsController(){
+        
+    }
+    
+    private Stage stage;
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+       //oculta mensaje de error
+        errorTextDocumentation.setVisible(false);
+        errorTextName.setVisible(false);
+        errorTextTel.setVisible(false);
+        errorTextEmail.setVisible(false);
+        errorTextNac.setVisible(false);
+        errorTextOc.setVisible(false);
+        errorTextCiv.setVisible(false);
+        
+        //cuando cualquier de los  textfield gane foco 
+        //si existe mensaje de error visible -> se oculta
+        
+        documnetation.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+             if (newValue){
+                errorTextDocumentation.setVisible(false);
+             }
+        });
+        
+        client.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+             if (newValue){
+                errorTextName.setVisible(false);
+             }
+        });
+        
+        tel.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+             if (newValue){
+                errorTextName.setVisible(false);
+             }
+        });
+        
+        email.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+             if (newValue){
+                errorTextName.setVisible(false);
+             }
+        });
+        
+        nac.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+             if (newValue){
+                errorTextName.setVisible(false);
+             }
+        });
+        
+        ocupation.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+             if (newValue){
+                errorTextName.setVisible(false);
+             }
+        });
+        
+        status.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+             if (newValue){
+                errorTextName.setVisible(false);
+             }
+        });
+    }
     public void setStage (Stage stage){
         this.stage = stage;
-    }
+    }   
+    
 }
