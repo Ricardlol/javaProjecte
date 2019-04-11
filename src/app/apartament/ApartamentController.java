75391820a49528e7f0ed516ae60a48ca75136fb6
Ricard <ricardlopezolivares@gmail.com>
2019-04-11 10:51:13 +0200

    edit view what did Jose Manuel

--- src/app/apartament/ApartamentController.java
+++ src/app/apartament/ApartamentController.java
@@ -5,9 +5,17 @@
  */
 package app.apartament;
 
+import app.model.Clients;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -16,7 +24,36 @@ import javafx.stage.Stage;
  * @author daw2
  */
 public class ApartamentController implements Initializable {
-
+    
+    // all TextFields in page
+    @FXML TextField nHab;
+    @FXML TextField piso;
+    @FXML TextField caract;
+    @FXML TextField email;
+    @FXML TextField nac;
+    @FXML TextField ocupation;
+    @FXML TextField status;
+    @FXML TextField nameSearch;
+    
+    // all Labels in page
+    @FXML Label errorTextDocumentation;
+    @FXML Label errorTextName;
+    @FXML Label errorTextTel;
+    @FXML Label errorTextEmail;
+    @FXML Label errorTextNac;
+    @FXML Label errorTextOc;
+    @FXML Label errorTextCiv;
+    @FXML Label errorTextIncorrectEmail;
+    
+    // menuButton in page
+    @FXML MenuButton optionDoc;
+    
+    // list in page
+    @FXML ListView listView;
+    
+    
+    private Clients clientobj;
+    
     private Stage stage;
     
     /**
@@ -25,6 +62,39 @@ public class ApartamentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+    
+     public void btnSave(){
+        clientobj.create(documnetation.getText(), client.getText(), tel.getText(), email.getText(), nac.getText(), ocupation.getText(), status.getText());
+        /*ocultarMensajes();
+        boolean campos = comprovarCampos();
+        if(campos){
+            System.out.println("A la base de datos !!!");
+        } else{
+            System.out.println("Pon los bien puto");
+        }*/
+    }
+    
+    public void btnModify(){
+        clientobj.modify(documnetation.getText(), client.getText(), tel.getText(), email.getText(), nac.getText(), ocupation.getText(), status.getText());
+    }
+    
+    public void btnDelete(){
+        clientobj.delete(documnetation.getText());
+    }
+    
+    public void btnSearch(){
+        ResultSet result = (ResultSet) clientobj.search(nameSearch.getText());
+        try {
+            while(result.next()) {
+                String name = result.getString("nombre");
+                System.out.println(name + "\n");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQLException"+ e.getMessage());
+            System.out.println("SQLState"+ e.getSQLState());
+            System.out.println("VendorError"+ e.getErrorCode());
+        }
     }
     
     public void setStage (Stage stage){
---
+++ src/app/model/Appartament.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author daw2
+ */
+public class Appartament implements Connectionsdb, actions{
+    private Connection conn;
+    private String sSQL="";
+    private Statement stmt = null;
+    private ResultSet rs = null;
+    private String tabla = "habitacion";
+    
+    private void connection(){
+        conn = Connectionsdb.connectarMySQL();
+        try{
+            stmt=conn.createStatement();
+        }catch(SQLException e){
+            System.out.println("SQLException"+ e.getMessage());
+            System.out.println("SQLState"+ e.getSQLState());
+            System.out.println("VendorError"+ e.getErrorCode());
+        }
+    }
+    
+    public void create(String documnetation, String client, String tel, String email, String nac, String ocupation, String status) {
+        connection();
+        sSQL ="INSERT INTO "+tabla+" VALUES ('"+documnetation+"', '"+client+"', '"+nac+"',"+tel+", '"+email+"', '"+ocupation+"', '"+status+"');";
+        try {
+            if(stmt.execute(sSQL)){
+                rs=stmt.getResultSet();
+                System.out.println("Cliente creado");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQLException"+ e.getMessage());
+            System.out.println("SQLState"+ e.getSQLState());
+            System.out.println("VendorError"+ e.getErrorCode());
+        }finally{
+            Connectionsdb.cerrarConnect(rs,stmt);
+        }
+    }
+
+    public void modify(String documnetation, String client, String tel, String email, String nac, String ocupation, String status) {
+        connection();
+        sSQL ="UPDATE "+tabla+" SET nombre ='"+client+"', nacionalidad='"+nac+"', telefono='"+tel+"', email='"+email+"', ocupacion='"+ocupation+"', estadoCivil='"+ status+"' WHERE documento='"+documnetation+"';";
+        try {
+            if(stmt.execute(sSQL)){
+                rs=stmt.getResultSet();
+                System.out.println("Cliente modificado");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQLException"+ e.getMessage());
+            System.out.println("SQLState"+ e.getSQLState());
+            System.out.println("VendorError"+ e.getErrorCode());
+        }finally{
+            Connectionsdb.cerrarConnect(rs,stmt);
+        }
+    }
+    
+    @Override
+    public Object search(String id) {
+        Object rsend=null;
+        connection();
+        sSQL ="SELECT * FROM "+tabla+" WHERE documento='"+id+"';";
+        try {
+            rs=stmt.executeQuery(sSQL);
+            rsend=rs;
+        } catch (SQLException e) {
+            System.out.println("SQLException"+ e.getMessage());
+            System.out.println("SQLState"+ e.getSQLState());
+            System.out.println("VendorError"+ e.getErrorCode());
+        }finally{
+            //Connectionsdb.cerrarConnect(rs,stmt);
+            return rsend;
+        }
+    }
+    
+    @Override
+    public void delete(String id) {
+        connection();
+        sSQL ="DELETE FROM "+tabla+" WHERE documento= '"+id+"';";
+        try {
+            if(stmt.execute(sSQL)){
+                rs=stmt.getResultSet();
+                System.out.println("Cliente Eliminado");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQLException"+ e.getMessage());
+            System.out.println("SQLState"+ e.getSQLState());
+            System.out.println("VendorError"+ e.getErrorCode());
+        }finally{
+            Connectionsdb.cerrarConnect(rs,stmt);
+        }
+    }
+}
